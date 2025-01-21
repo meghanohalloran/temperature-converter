95099dc0e7438e49c2a8e466ea4d3324391db298
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-20 20:57:13 -0700

    hours of work and still cant get equation to popuate 1-20-25

---
+++ index.js
@@ -0,0 +1,11 @@
+
+window.alert("Input a Farenheit temperature value and we shall convert it to Celsius");
+
+let num1 = parseInt(prompt("your temperature in Celsius is"));
+
+let result = Number(num1);
+
+
+
+
+
