58b800d194a46cc5ec8c67af2b279f304fa92000
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-21 19:39:00 -0700

    updated changes from  second update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-window.alert("Input a Farenheit temperature value and we shall convert it to Celsius");
+window.alert("Input a Farenheit temperature value and this will convert it to Celsius");
 
-let num1 = parseInt(prompt("your temperature in Celsius is"));
+const tempNumberInput = prompt(`Input your temperature`);
 
-let result = Number(num1);
+const add = (convertToCelcius) => {(tempNumberInput - 32) * 5/9};
+
+const convertToCelcius = (`this is your temperature in celsius`);
 
 
+//console.log("your temperature is" {convertToCelsius});
+//prompt the user to input a f temp and let them know 
 
+//let num2 = parseInt(prompt("your temperature in Celsius is"));
 
+//const num2 = 
 
