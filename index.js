de4196f56845c95af2ed536b724f719f85af1918
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-22 18:05:37 -0700

    final changes to push up

--- index.js
+++ index.js
@@ -1,13 +1,106 @@
 
 
-(prompt)("Input a Farenheit temperature value and this will convert it to Celsius");
+let fahrenheit = prompt("Input a Farenheit temperature value and this will convert it to Celsius");
 
-function convertFarenheittoCelsius(num) {
-    let celsius = prompt ("Input a Farenheit temperature and this will convert it to Celsius:");
-    const fahrenheit: number = ((celsius - 32) * (5/9));
+function convertToCelsius() {
 
-    alert(`${celsius} is ${fahrenheit} This is your temperature in Celsius: ${fahrenheit}`);
+  const result = (fahrenheit - 32) * (5/9);
+
+  
+alert("your temp is"+ result);
+
+if (result <32) {
+  alert ("verycold");
+} else if (result <64) {
+  alert ("cold");
+} else if (result <86) {
+  alert ("warm");
+} else if (result <100) {
+  alert ("hot");
+} else if (result >= 100) {
+  alert ("very hot");
+} else {
+  alert ("this is not a temp");
 }
+
+
+} 
+convertToCelsius()
+
+// if (result <32) {
+//   alert ("verycold");
+// } else if (result <64);
+//   alert ("cold");
+// } else if (result <86);
+// alert ("warm");
+// } else if (result <100);
+// alert ("hot");
+// if (result >=100);
+// alert ("very hot");
+
+// Const num = "cold";
+
+// // if key word (conditional statement){ code }
+// // else statements do not have a conditional statement
+// if (num < 32) {
+//   alert("its very cold");
+// } else if (num < 64) {
+//   alert("a little chilly");
+// } else if (num >= 100) {
+//   alert("Its wayy too hot");
+// } else {
+//   alert("this is not a temp");
+// }
+// 
+
+
+
+
+
+//  function describeTemperature() {
+ 
+
+
+ 
+  
+
+//  }
+
+
+
+
+
+
+
+
+
+// prompt the user to input a f temp and let them know we will convert to Celsius
+// create a function that called convertToCelsius  that takes in the f input and returns the Celsius temp
+//then...
+// create a function called describe Temperature which will take in a f temp and return a description based on the following
+//  temp   ->   descrip
+//  <32    ->   "very cold"
+//  <64    ->   "cold"
+//  <86    ->   "warm" 
+//  <100   ->   "hot"
+//  >=100  ->   "very hot" 
+
+
+  // return -> convertToCelsius;
+
+  __________________________________________
+
+
+  
+  
+  
+  // let Celsius = prompt("Input a temperature in Farenheit and this will convert it to Celsius:");
+  
+  // 
+  // function =>
+  // console.log(`good`);
+
+    // alert(`${celsius} is ${fahrenheit} This is your temperature in Celsius: ${fahrenheit}`);
 
  
 // const tempNumberInput = prompt(`Input your temperature`);
@@ -20,13 +113,6 @@ function convertFarenheittoCelsius(num) {
 
 // let temp = 
 // const convertToCelcius = (`this is your temperature in celsius`);
-
-
-
-//prompt the user to input a f temp and let them know we will convert to Celsius
-// create a function that called convertToCelsius  that takes in the f input and returns the Celsius
-
-
 
 //let num2 = (prompt("your temperature in Celsius is"));
 
